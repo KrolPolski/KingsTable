0bbd98c7d94668fa4148d7fe7c0ad91369066007
Ryan Boudwin <rboudwin@student.hive.fi>
2024-11-04 15:14:59 +0200

    Now drawing current map with piece positions

--- src/Map.cpp
+++ src/Map.cpp
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 11:12:44 by rboudwin          #+#    #+#             */
-/*   Updated: 2024/11/04 15:00:44 by rboudwin         ###   ########.fr       */
+/*   Updated: 2024/11/04 15:13:46 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,10 @@ void Map::drawBoard()
 void Map::drawPieces()
 {
 	std::vector<sf::CircleShape> attackers(16);
+	std::vector<sf::CircleShape> defenders(8);
+	sf::CircleShape myLiege(40, 6);
 	int attackerIndex {0};
+	int defenderIndex {0};
 	for (size_t i = 0; i < 9; i++)
 	{
 		for (size_t k = 0; k < 9; k++)
@@ -64,10 +67,30 @@ void Map::drawPieces()
 				attackers[attackerIndex].setFillColor(sf::Color(0, 0, 0));
 				attackers[attackerIndex].setOutlineColor(sf::Color(200,200,200));
 				attackers[attackerIndex].setOutlineThickness(2);
-				attackers[attackerIndex].setPosition(58 + k * 100, 58 + i * 100);
+				attackers[attackerIndex].setPosition(58 + k * 100, 58 + i * 100); //50 for edge of board, 8 to center pieces based on the square outlines being 4 on each side
 				mapWindow->draw(attackers[attackerIndex]);
 				attackerIndex++;	
 			}
+			else if (curr_map[i][k] == 'D')
+			{
+				defenders[defenderIndex].setRadius(40);
+				defenders[defenderIndex].setPointCount(5);
+				defenders[defenderIndex].setFillColor(sf::Color(255,255,255));
+				defenders[defenderIndex].setOutlineColor(sf::Color(50,50,50));
+				defenders[defenderIndex].setOutlineThickness(2);	
+				defenders[defenderIndex].setPosition(58 + k * 100, 58 + i * 100);	
+				mapWindow->draw(defenders[defenderIndex]);
+				defenderIndex++;
+			}
+			else if (curr_map[i][k] == 'K')
+			{
+				myLiege.setFillColor(sf::Color(255, 245, 30));
+				myLiege.setOutlineColor(sf::Color(50, 50, 50));
+				myLiege.setOutlineThickness(2);
+				myLiege.setPosition(58 + k * 100, 58 + i * 100);
+				mapWindow->draw(myLiege);
+			}
+			
 		}
 	}
 }
